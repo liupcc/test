bd96d312ad41117a1fafeaeb1ea6ccef93d79b83
liupcc <820029166@qq.com>
2020-04-24 17:07:25 +0800

    test

---
+++ src/main/java/com/liup/study/Test.java
@@ -0,0 +1,17 @@
+package com.liup.study;
+
+public class Test
+{
+	
+	
+	private String aa;
+	
+	private String tt;
+	
+	private String rr;
+	
+	public static void main(String[] args)
+	{
+		System.out.println();
+	}
+}
